64027c4169160a7a98b0f3049237265d6abd62ef
20A31A0582 <109906715+20A31A0582@users.noreply.github.com>
2023-02-03 09:51:42 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -42,3 +42,60 @@ print(s1.strip())#removes sapce at beg and end
 a="hajdjnoij327389&jkj"
 print(a.split("&")) #splits at specified pos and ret out as list  ['hajdjnoij327389', 'jkj']
 print(a.center(60,'$'))# width,filled character
+
+#mutable--list,set,tuple
+#immutable--int,float,string,bool,tuple
+
+
+#get one string as input along with a character find out and display whether the particular characater is in string or not
+s=list(map(str,input("Enter:").split(" ")))
+if s[1] in s[0]:
+    print("Yes")
+else:
+    print("NO")
+#another process
+s=input("Enter:")
+c=input("Enter:")
+f=0
+for i in s:
+    if i==c:
+        f=1
+        #break
+    else:
+        f=0
+if(f==1):
+    print('Yes')
+else:
+    print('No')
+#check palindrome or not
+
+s=input("Enter:") 
+r=s[::-1]
+if s==r:
+    print("Yes.It is a palindrome")
+else:
+    print("No")
+#check given string contains space or not i yes count no of spaces and print
+s=input("Enter:")
+c=0
+for i in s:
+    if i==" ":
+        c=c+1
+print("{} occurs {} times".format('space',c))
+#create a list with vowels  get string as input count vowels from string
+v=['a','e','i','o','u','A','E','I','O','U']
+c=0
+s=input("Enter:")
+for i in s:
+    if i in v:
+        c=c+1
+print(c)
+#math
+import math
+print("ceil value:",math.ceil(12.34))
+print("floor value",math.floor(12.34))
+print("factorial:",math.factorial(3))
+print("Power:",math.pow(2,5))
+print("Modulator:",math.fmod(20,3))
+a,b=divmod(10,3)
+print(a,b)
