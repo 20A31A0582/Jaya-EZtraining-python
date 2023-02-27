6dcc17dfd23187c7a1f8e41c1de5c7437fa12b60
20A31A0582 <109906715+20A31A0582@users.noreply.github.com>
2023-02-27 10:29:47 +0530

    Update Day8.py

--- Day8.py
+++ Day8.py
@@ -314,70 +314,7 @@ obj.display()
 
 
 
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-class LinkedList:
-    def __init__(self):
-        self.head = None
-    def printlist(self):
-        temp=self.head
-        if not temp:
-            print("Linked list is empty")
-            return
-        else:
-            print("Start:",end=" ")
-        while temp:
-            print(temp.data,end="->")
-            temp=temp.next
-        print("End")
-    def insert(self,data):
-       new_node=Node(data)
-       if self.head is None:
-           self.head=new_node
-       elif self.head.data>=new_node.data:
-           new_node.next=self.head
-           self.head=new_node
-       else:
-          current=self.head
-          while current.next and new_node.data:
-              current=current.next
-          new_node.next=current.next
-          current.next-new_node
-   def delete(self,key):
-       if self.head is None:
-          print("Deletion not possible")
-          return
-       if self.head.data==Key:
-          self.head=self.head.next
-          return
-       current=self.head
-       while current:
-           if current.data==key:
-               break
-          previous=current
-          current=current.next
-     if current is None:
-         print("Deletion error:key not found")
-     else:
-         previous.next=current.next
-if __name__=='__main__':
-    obj=LinkedList()
-    print('')
-    obj.insert(10)
-    obj.insert(12)
-    obj.insert(8)
-    obj.insert(11)
-    obj.insert(10)
-    obj.printlist()
-    obj.delete(12)
-    obj.delete(10)
-    obj.delete(8)
-    obj.printlist()
-         
-
+ 
 
 
 
