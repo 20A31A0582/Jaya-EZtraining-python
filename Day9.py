7fc5436a2d9dc9dcf0a68b2186b1f3066f861f86
20A31A0582 <109906715+20A31A0582@users.noreply.github.com>
2023-02-27 14:38:00 +0530

    Update Day9.py

--- Day9.py
+++ Day9.py
@@ -60,3 +60,211 @@ if __name__=='__main__':
     obj.delete(8)
     obj.printlist()
          
+
+print("Before function")
+def f1():
+    print("f1")
+def f2():
+    print("f2")
+def f3():
+    print("f3")
+if __name__=='__main__':
+    f1()
+    f2()
+    f3()
+
+#DOUBLY LINKED LIST 
+#INSERTING AT START
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev=None
+        self.next = None
+class dLinkedList:
+    def __init__(self):
+        self.head = None
+    def insert_start(self):
+        n=Node(300)
+        temp=self.head
+        temp.prev=n
+        n.next=temp
+        self.head=n
+    def display(self):
+        temp=self.head
+        if not temp:
+            print("Linked list is empty")
+            return
+        else:
+            print("Start:",end=" ")
+        while temp:
+            print(temp.data,end="->")                                                                                  
+            temp=temp.next
+        print("End")
+     
+if __name__=='__main__':
+    obj=dLinkedList()
+    n1=Node(100)
+    obj.head=n1
+    n2=Node(200)
+    n2.prev=n1
+    n1.next=n2
+    obj.insert_start()
+    obj.display()
+
+
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev=None
+        self.next = None
+class dLinkedList:
+    def __init__(self):
+        self.head = None
+    def insert_end(self):
+        n=Node(300)
+        temp=self.head
+        while temp.next is not None:
+            temp=temp.next
+        temp.next=n
+        n.prev=temp         
+    def display(self):
+        temp=self.head
+        if not temp:
+            print("Linked list is empty")
+            return
+        else:
+            print("Start:",end=" ")
+        while temp:
+            print(temp.data,end="->")                                                                                  
+            temp=temp.next
+        print("End")
+     
+if __name__=='__main__':
+    obj=dLinkedList()
+    n1=Node(100)
+    obj.head=n1
+    n2=Node(200)
+    n2.prev=n1
+    n1.next=n2
+    obj.insert_end()
+    obj.display()
+   
+
+
+#insert at pos
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev=None
+        self.next = None
+class dLinkedList:
+    def __init__(self):
+        self.head = None
+    def insert_pos(self,pos):
+        n=Node(300)
+        temp=self.head
+        for i in range(1,pos-1):
+            temp=temp.next
+        n.prev=temp
+        n.next=temp.next
+        temp.next.prev=n
+        temp.next=n
+    def display(self):
+        temp=self.head
+        if not temp:
+            print("Linked list is empty")
+            return
+        else:
+            print("Start:",end=" ")
+        while temp:
+            print(temp.data,end="->")                                                                                  
+            temp=temp.next
+        print("End")
+     
+if __name__=='__main__':
+    obj=dLinkedList()
+    n1=Node(100)
+    obj.head=n1
+    n2=Node(200)
+    n2.prev=n1
+    n1.next=n2
+    obj.insert_pos(1)
+    obj.display()
+
+
+#Deletion
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev=None
+        self.next = None
+class dLinkedList:
+    def __init__(self):
+        self.head = None
+    def delete_begining(self):
+        temp=self.head
+        self.head=temp.next
+        temp.next=None
+    def display(self):
+        temp=self.head
+        if not temp:
+            print("Linked list is empty")
+            return
+        else:
+            print("Start:",end=" ")
+        while temp:
+            print(temp.data,end="->")                                                                                  
+            temp=temp.next
+        print("End")
+     
+if __name__=='__main__':
+    obj=dLinkedList()
+    n1=Node(100)
+    obj.head=n1
+    n2=Node(200)
+    n2.prev=n1
+    n1.next=n2
+    obj.delete_begining()
+    obj.display()
+
+
+#del at end
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev=None
+        self.next = None
+class dLinkedList:
+    def __init__(self):
+        self.head = None
+    def delete_end(self):
+        temp=self.head
+        while temp.next is not None:
+            temp=temp.next
+        temp.prev.next=None
+        temp.prev=None
+    def display(self):
+        temp=self.head
+        if not temp:
+            print("Linked list is empty")
+            return
+        else:
+            print("Start:",end=" ")
+        while temp:
+            print(temp.data,end="->")                                                                                  
+            temp=temp.next
+        print("End")
+     
+if __name__=='__main__':
+    obj=dLinkedList()
+    n1=Node(100)
+    obj.head=n1
+    n2=Node(200)
+    n2.prev=n1
+    n1.next=n2
+    obj.delete_end()
+    obj.display()
