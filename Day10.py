4288ca1ebd673d06852d983bec5944270394620d
20A31A0582 <109906715+20A31A0582@users.noreply.github.com>
2023-02-28 14:48:47 +0530

    Update Day10.py

--- Day10.py
+++ Day10.py
@@ -1,5 +1,5 @@
 
-"""#STACK
+#STACK
 #USING LiST
 stack=[]
 def push():
@@ -124,7 +124,7 @@ while True:
         display()
     else:
         break
-"""
+
 
 print("queue using queue module")
 import queue
@@ -147,3 +147,50 @@ print(s.get())
 print(s.get())
 print(s.get())
 print(s.empty())
+
+
+
+class Node:
+    def __init__(self, data):
+       self.data = data
+       self.next = None
+ 
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.last = None
+ 
+    def enqueue(self, data):
+        if self.last is None:
+            self.head = Node(data)
+            self.last = self.head
+        else:
+            self.last.next = Node(data)
+            self.last = self.last.next
+ 
+    def dequeue(self):
+        if self.head is None:
+            return None
+        else:
+            to_return = self.head.data
+            self.head = self.head.next
+            return to_return
+ 
+a= Queue()
+while True:
+    print('enqueue <value>')
+    print('dequeue')
+    print('quit')
+    do = input('What would you like to do? ').split()
+ 
+    operation = do[0].strip().lower()
+    if operation == 'enqueue':
+        a.enqueue(int(do[1]))
+    elif operation == 'dequeue':
+        dequeued = a.dequeue()
+        if dequeued is None:
+            print('Queue is empty.')
+        else:
+            print('Dequeued element: ', int(dequeued))
+    elif operation == 'quit':
+        break
